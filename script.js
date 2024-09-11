ac1772ad88ae94c5826d4462174986cc950eeaef
Alzzo2024 <simaoaraujo987@gmail.com>
2024-09-11 16:33:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const demoBtn = document.getElementById('demo-btn');
     const downloadsCountElem = document.getElementById('downloads-count');
-    
+
     // Recupera o número de downloads do localStorage ou inicializa como 0
     let downloadsCount = parseInt(localStorage.getItem('downloadsCount')) || 0;
     downloadsCountElem.textContent = downloadsCount;
 
     demoBtn.addEventListener('click', (event) => {
         event.preventDefault();
-        
+
         // Atualiza o contador de downloads
         downloadsCount += 1;
         localStorage.setItem('downloadsCount', downloadsCount);
         downloadsCountElem.textContent = downloadsCount;
 
-        // Redireciona para o site de destino
-        window.location.href = 'https://www.instagram.com/ametysta.official/'; // Substitua pelo URL desejado
+        // Inicia o download e redireciona para o arquivo
+        const downloadUrl = ''; // Substitua pela URL do arquivo que você deseja que seja baixado
+        window.location.href = downloadUrl;
     });
 });
