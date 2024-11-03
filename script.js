c9f63260bc7ca532f9f7c5d3e0d93b7524812e71
Alzzo2024 <simaoaraujo987@gmail.com>
2024-11-03 17:53:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const downloadsCountElement = document.getElementById('downloads-count');
-    const downloadBtn = document.getElementById('downloadBtn');
+    const androidDownloadBtn = document.getElementById('androidDownloadBtn');
+    const appleDownloadBtn = document.getElementById('appleDownloadBtn');
     const themeSelector = document.getElementById('themeSelector');
     const sybillaLink = document.getElementById('sybillaLink');
     const sybillaAlert = document.getElementById('sybillaAlert');
@@ -24,8 +25,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // Inicializa o contador de downloads
     updateDownloadsCount();
 
-    // Adiciona o evento de clique no botão de download
-    downloadBtn.addEventListener('click', (event) => {
+    // Adiciona o evento de clique no botão de download do Android
+    androidDownloadBtn.addEventListener('click', (event) => {
         event.preventDefault();
         
         fetch('https://api.jsonbin.io/v3/b/66e7113dacd3cb34a884f597', {
@@ -49,11 +50,19 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .then(() => {
                 updateDownloadsCount();
+                // Redireciona para o link do APK
                 window.location.href = 'https://storage.googleapis.com/appilder/app/efb2d658839ae545ba85a93a515f2d89/fe0e6b315d91c71dae0244d3163c96fa.apk?GoogleAccessId=desktopapi%40appilder-com.iam.gserviceaccount.com&Expires=1726620991&Signature=JC4ci4KPfUtLRoBM0csdS0aYjmG8iaDhTwoZ%2FZd%2FyWjDjzsUvqLAChuTcwtYuJaarM6AMQXqKshl7%2FW49EB0BXoMgQL9Ce5oZHuiV1JE4z%2BaY7KCdjxQFqokiPcgGks34SzfIwC9d4BPRKoFzC0phGLKlom289kt0144g5QijFjzlEEi7yAGc5YFYp1kBLJ4ak6BP7I9psE5g87I9jtvEnlzqHp4rDE%2BjqepazJcGKTvFyU8UuFtQ%2BXOxfwnvajmkqe8LksPaZbcxQDDL1olTuZ2F%2BrI1MCIrwQCTNjUIDlGJv6XvdiewnEroQhPF1irrGhZj%2B0bwVSUmmUj3%2BpOsQ%3D%3D';
             })
             .catch(error => console.error('Error updating download count:', error));
         })
         .catch(error => console.error('Error fetching current download count:', error));
+    });
+
+    // Adiciona o evento de clique no botão de download da Apple
+    appleDownloadBtn.addEventListener('click', (event) => {
+        event.preventDefault();
+        // Aqui você pode adicionar a lógica de download para a Apple
+        alert('Funcionalidade de download para Apple ainda não implementada.');
     });
 
     // Seletor de tema
